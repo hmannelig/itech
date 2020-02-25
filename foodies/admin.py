0481c86cedb8f8fb7b03c1580e577ed559915ea1
Linh <2444965H@student.gla.ac.uk>
2020-02-25 15:49:37 +0000

    Changed from "Page" to "Meal" + Category Buttons
    models.py: Added Foreign Keys for Cateogry & Meals, adapted attributes;
    urls.py: change from page to meal;
    views.py: change from page to meal;
    foodies.py: change from page to meal, added static links to meal pages for meal categories, added url variable;
    add_category.html: change from page to meal;
    categories.html: Added button;
    category.html: change from page to meal;
    index.html: change from page to meal

--- foodies/admin.py
+++ foodies/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from foodies.models import Category, Page, UserProfile, Ingredient, Allergy
+from foodies.models import Category, Meal, UserProfile, Ingredient, Allergy
 
-class PageAdmin(admin.ModelAdmin):
+class MealAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
+admin.site.register(Meal, MealAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Ingredient)
 admin.site.register(Allergy)
