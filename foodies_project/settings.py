15b890a15a6cdeda2981ca2b7ac44666c1d23798
efrain2010 <efrain.villanueva3@gmail.com>
2020-03-20 13:13:28 +0000

    Fixed base.html templatehe body tag was missing, The body tag was missing The content of the page was missplaced There were repeated imported links and scripts

--- foodies_project/settings.py
+++ foodies_project/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = '@v9*m_19^%ql1f3d3az4wuj7*2uv9=05i#nt0xwjpvdc7l62ib'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'efra.pythonanywhere.com',
+    '127.0.0.1',
+]
 
 # Application definition
 
