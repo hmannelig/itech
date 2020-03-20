258a0cd8e1590959abd4ff11fd4a6023fe1ad87e
efrain2010 <efrain.villanueva3@gmail.com>
2020-03-20 16:12:37 +0000

    Fixed the are you cooker and are you diner fields on the sing up form

--- foodies/views.py
+++ foodies/views.py
@@ -17,9 +17,6 @@ def index(request):
     visitor_cookie_handler(request)
     return render(request, 'foodies/index.html', context=context_dict)
 
-
-
-
 def about(request):
     context_dict = {}
     visitor_cookie_handler(request)
@@ -28,35 +25,20 @@ def about(request):
 
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
 
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
+        
         meals = Meal.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
 
         context_dict['meals'] = meals
 
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything -
-        # the template will display the "no category" message for us.
         context_dict['category'] = None
         context_dict['meals'] = None
-    # Go render the response and return it to the client.
+
     return render(request, 'foodies/category.html', context=context_dict)
 
 
@@ -64,23 +46,16 @@ def show_category(request, category_name_slug):
 def add_category(request):
     form = CategoryForm()
 
-    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
+
             return redirect('/foodies/')
         else:
-            # The supplied form contained errors -
-            # just print them to the terminal.
             print(form.errors)
-        # Will handle the bad form, new form, or no form supplied cases.
-        # Render the form with error messages (if any).
+
     return render(request, 'foodies/add_category.html', {'form': form})
 
 
@@ -138,8 +113,9 @@ def register(request):
         # If the two forms are valid...
         if user_form.is_valid() and profile_form.is_valid():
             # Save the user's form data to the database.
-            if request.POST(isCooker) is False and request.POST(isDinner) is False:
-                return redirect('foodies/register/')
+            data = request.POST.copy()
+            if data.get('isCooker') == None and data.get('isDinner') == None:
+               return redirect('foodies/register/')
 
             user = user_form.save()
 
---
+++ static/foodies/js/foodies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function sendLoginDetails(e) {
+    var userName = $('#login_form [name=username]').val();
+    var password = $('#login_form [name=password]').val();
+    if(userName == '' || userName == null || password == '' || password  == null) {
+        alert('Write your user name and password');
+        return false;
+    }
+
+    e.target.submit();
+}
+
+$('#login_form').submit(function(e) {
+    e.preventDefault();
+    sendLoginDetails(e);
+});
