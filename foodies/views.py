19118d22f9e8c1cebebf0bc1e3c0d3cf82f45822
Elpida Vasiou <2487177v@student.gla.ac.uk>
2020-03-22 17:05:17 +0000

    updated search

--- foodies/views.py
+++ foodies/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from foodies.bing_search import run_query
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -276,3 +277,14 @@ def contact_us(request):
 def request(request):
     return render(request, 'foodies/request.html')
 
+def search(request):
+    result_list = []
+    query = ''
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            result_list = run_query(query)
+    
+    return render(request, 'foodies/search.html', {'result_list': result_list, 'query': query})
