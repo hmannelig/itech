07bdccec86145a781b43f3445315a35a58e8e638
Pablo Berjon <2490772B@student.gla.ac.uk>
2020-03-23 00:45:54 +0000

    Fixed add meal rediret

--- foodies/views.py
+++ foodies/views.py
@@ -79,11 +79,11 @@ def add_meal(request):
             ingredient.save()
             meal.save()
 
-            cleaned_data = ingredientsMeal['meal'].cleaned_data
-            cleaned_data = cleaned_data['category'].name.lower()
-            category = '/category/' + cleaned_data + '/'
+            # cleaned_data = ingredientsMeal['meal'].cleaned_data
+            # cleaned_data = cleaned_data['category'].name.lower()
+            # category = '/category/' + cleaned_data + '/'
 
-            return redirect(category)
+            return redirect('foodies/add_meal.html')
         else:
             return HttpResponse("Something went wrong")
     else:
